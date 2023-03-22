95b77f4e22e964b18efdda6eb2e8d844407ba204
julienvankrinkelen <julienvankrinkelen@gmail.com>
2023-03-22 10:14:24 +0100

    Fix Plateforme bug + Music Slider Opé
    Reste à faire :
    - Pref de toggle Full Screen et Music general sur tous les menus
    - Load Save game sur statue link avec start menu (attention collectibles)

---
+++ GAME (Prankex)/Assets/Scripts/VolumeMusic.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class VolumeMusic : MonoBehaviour
+
+{
+     public AudioSource audio;
+// Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float volumeMusicValue = PlayerPrefs.GetFloat("VolumeMusicValue");
+        audio.volume = volumeMusicValue;
+    }
+}
